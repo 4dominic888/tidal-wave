bfb8ea00a34a4503567c5c4c4b02b248ec466d62
4dominic888 <4dominin888@gmail.com>
2024-04-30 08:48:00 -0500

    reproductor musica module agregado

---
+++ lib/modules/reproductor_musica/screens/reproductor_musica_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReproductorMusicaScreen extends StatefulWidget {
+  const ReproductorMusicaScreen({super.key});
+
+  @override
+  State<ReproductorMusicaScreen> createState() => _ReproductorMusicaScreenState();
+}
+
+class _ReproductorMusicaScreenState extends State<ReproductorMusicaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

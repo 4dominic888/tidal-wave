4e6fe557ff4459d8de38c9335676051ad0b17e6b
4dominic888 <4dominin888@gmail.com>
2024-04-30 08:47:19 -0500

    lista musica module agregado

---
+++ lib/modules/lista_musica/screens/lista_musica_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListaMusicaScreen extends StatefulWidget {
+  const ListaMusicaScreen({super.key});
+
+  @override
+  State<ListaMusicaScreen> createState() => _ListaMusicaScreenState();
+}
+
+class _ListaMusicaScreenState extends State<ListaMusicaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

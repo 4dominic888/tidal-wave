8d78e9f1f2efba0abac5a0a30a628e2636e267e8
4dominic888 <4dominin888@gmail.com>
2024-05-05 01:05:05 -0500

    musica item

--- lib/modules/lista_musica/screens/lista_musica_screen.dart
+++ lib/modules/lista_musica/screens/lista_musica_screen.dart
@@ -1,9 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tidal_wave/modules/lista_musica/widgets/icon_button_music.dart';
+import 'package:tidal_wave/modules/lista_musica/widgets/music_item.dart';
 import 'package:tidal_wave/modules/lista_musica/widgets/text_field_find.dart';
+import 'package:tidal_wave/modules/reproductor_musica/classes/musica.dart';
 
 class ListaMusicaScreen extends StatefulWidget {
-  const ListaMusicaScreen({super.key});
+
+  final List<Music> listado;
+
+  const ListaMusicaScreen({super.key, required this.listado});
 
   @override
   State<ListaMusicaScreen> createState() => _ListaMusicaScreenState();
@@ -53,7 +62,7 @@ class _ListaMusicaScreenState extends State<ListaMusicaScreen> {
         elevation: 1,
       ),
       body: Container(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.only(top: 10),
         height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(
@@ -62,6 +71,26 @@ class _ListaMusicaScreenState extends State<ListaMusicaScreen> {
             end: Alignment.bottomCenter,
             colors: [Color.fromARGB(255, 30, 114, 138),Color(0xFF071A2C)]
           )
+        ),
+        
+        child: Stack(
+          children: [
+            Positioned(
+              right: 10,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: widget.listado.length,
+                  itemBuilder: (context, index) {
+                        
+                    final musica = widget.listado[index];
+                    return MusicItem(music: musica);
+                }),
+              ),
+            ),
+          ],
         ),
       )
     );

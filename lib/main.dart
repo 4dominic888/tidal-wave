0f662f5823558a2520689f32209e50dc44df6699
4dominic888 <4dominin888@gmail.com>
2024-05-08 20:44:31 -0500

    usando bloc en musica

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:tidal_wave/bloc/music_cubit.dart';
 import 'package:tidal_wave/modules/lista_musica/screens/lista_musica_screen.dart';
 import 'package:tidal_wave/modules/reproductor_musica/classes/musica.dart';
 
@@ -47,18 +49,24 @@ class MyApp extends StatelessWidget {
 
     List<Music> duplicatedList = List.generate(16, (_) => playListTest).expand((element) => element).toList();
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      
-      //? Codigo para cambiar la bolita esa de los textos
-      theme: ThemeData(
-        textSelectionTheme: TextSelectionThemeData(
-          selectionHandleColor: Colors.grey.shade400,
-          selectionColor: Colors.grey.shade400.withOpacity(0.4),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => MusicCubit())
+      ],
+
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        
+        //? Codigo para cambiar la bolita esa de los textos
+        theme: ThemeData(
+          textSelectionTheme: TextSelectionThemeData(
+            selectionHandleColor: Colors.grey.shade400,
+            selectionColor: Colors.grey.shade400.withOpacity(0.4),
+          ),
         ),
+        home: ListaMusicaScreen(listado: duplicatedList),
       ),
-      home: ListaMusicaScreen(listado: duplicatedList),
     );
   }
 }

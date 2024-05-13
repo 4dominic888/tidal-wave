15b565c270e957798047f09255fa1bf8331a42a1
4dominic888 <4dominin888@gmail.com>
2024-05-13 09:25:02 -0500

    refactorizacion de la actualizacion de color del background

--- lib/shared/color_util.dart
+++ lib/shared/color_util.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 class ColorUtil{
+
+  /// Oscurece el color dado, en base al amount colocado
   static Color darken(Color color, {double amount = .1}){
     assert(amount >= 0 && amount <= 1);
 
@@ -8,5 +12,26 @@ class ColorUtil{
     final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
 
     return hslDark.toColor();
+  }
+
+  /// Return a Dominan color & Constrast color in a array
+  static Future<List<dynamic>> adjustColorBy({String? imgUrl}) async {
+    
+    final List<Color> dominanColors = [ const Color.fromARGB(255, 30, 114, 138),const Color(0xFF071A2C)];
+    final Color constrastColor;
+
+    if(imgUrl != null){
+      final PaletteGenerator paletteGenerator = await PaletteGenerator.fromImageProvider(
+        CachedNetworkImageProvider(imgUrl),
+        size: const Size(300, 300),
+        maximumColorCount: 2
+      );
+      final int paletteLen = paletteGenerator.colors.length;
+      for (int i = 0; i < paletteLen; i++) {
+        dominanColors[i] = paletteGenerator.colors.elementAt(i);
+      }
+    }
+    constrastColor = dominanColors.last.computeLuminance() > 0.5 ? Colors.black : Colors.white;
+    return [dominanColors, constrastColor];
   }
 }

9cb03bf9b2a666a5ca982f34fcbcf5bf0aa1d7fb
4dominic888 <4dominin888@gmail.com>
2024-05-17 00:21:14 -0500

    control de musica agregado

--- lib/modules/reproductor_musica/widgets/controls.dart
+++ lib/modules/reproductor_musica/widgets/controls.dart
@@ -1,18 +1,27 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:super_tooltip/super_tooltip.dart';
+import 'package:tidal_wave/modules/reproductor_musica/widgets/volume_control.dart';
 import 'package:tidal_wave/shared/music_state_util.dart';
 
-class Controls extends StatelessWidget {
+class Controls extends StatefulWidget {
   final AudioPlayer audioPlayer;
   static const int seconds = 10;
   final Color color;
+
+
   const Controls({super.key, required this.audioPlayer, required this.color});
 
+  @override
+  State<Controls> createState() => _ControlsState();
+}
+
+class _ControlsState extends State<Controls> {
   Widget _buttonBuilder(Icon icon, double size, void Function() onPressed, {bool locked = false}){
     return IconButton(
       icon: icon,
-      color: locked ? color.withOpacity(0.3) : color,
+      color: locked ? widget.color.withOpacity(0.3) : widget.color,
       iconSize: size,
       onPressed: onPressed,
       enableFeedback: true
@@ -20,24 +29,26 @@ class Controls extends StatelessWidget {
   }
 
   Future<void> seekRewind() async{
-    await audioPlayer.seek(audioPlayer.position - const Duration(seconds: seconds));
+    await widget.audioPlayer.seek(widget.audioPlayer.position - const Duration(seconds: Controls.seconds));
   }
 
   Future<void> seekForward() async{
-    await audioPlayer.seek(audioPlayer.position + const Duration(seconds: seconds));
+    await widget.audioPlayer.seek(widget.audioPlayer.position + const Duration(seconds: Controls.seconds));
   }
 
   Future<void> restart() async {
-    await audioPlayer.seek(Duration.zero);
+    await widget.audioPlayer.seek(Duration.zero);
   }
 
   Future<void> random() async {
     final random = Random();
-    final int maxIndex = audioPlayer.sequence!.length;
+    final int maxIndex = widget.audioPlayer.sequence!.length;
     final int randomIndex = random.nextInt(maxIndex);
-    await audioPlayer.seek(Duration.zero, index: min(randomIndex, maxIndex));
+    await widget.audioPlayer.seek(Duration.zero, index: min(randomIndex, maxIndex));
 
   }  
+
+  final _superToolTipController = SuperTooltipController();
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +63,14 @@ class Controls extends StatelessWidget {
             Expanded(
               child: Container(),
             ),
-            Container(child: _buttonBuilder(const Icon(Icons.shuffle_rounded), 30, random))
+            Container(child: _buttonBuilder(const Icon(Icons.shuffle_rounded), 30, random)),
+
+            // Volumen
+            VolumeControl(
+              audioPlayer: widget.audioPlayer,
+              buttonBuilder: _buttonBuilder,
+            ),
+            
           ]
         ),
 
@@ -65,31 +83,31 @@ class Controls extends StatelessWidget {
         
             //* Previous button
             Expanded(child: StreamBuilder<int?>(
-              stream: audioPlayer.currentIndexStream,
+              stream: widget.audioPlayer.currentIndexStream,
               builder: (context, snapshot) {
                 return MusicStateUtil.previousReturns<Widget>(snapshot.data,
                   noActive: _buttonBuilder(const Icon(Icons.skip_previous_rounded), 60, (){}, locked: true),
-                  active: _buttonBuilder(const Icon(Icons.skip_previous_rounded), 60, audioPlayer.seekToPrevious)
+                  active: _buttonBuilder(const Icon(Icons.skip_previous_rounded), 60, widget.audioPlayer.seekToPrevious)
                 );
               })
             ),
             
             //* Play/Stop
             StreamBuilder<PlayerState>(
-              stream: audioPlayer.playerStateStream,
-              builder: (context, snapshot) => _buttonBuilder(MusicStateUtil.playIcon(snapshot.data), 80, MusicStateUtil.playAction(audioPlayer))
+              stream: widget.audioPlayer.playerStateStream,
+              builder: (context, snapshot) => _buttonBuilder(MusicStateUtil.playIcon(snapshot.data), 80, MusicStateUtil.playAction(widget.audioPlayer))
             ),
         
             //* Skip button
             Expanded(
               child: StreamBuilder<int?>(
-              stream: audioPlayer.currentIndexStream,
+              stream: widget.audioPlayer.currentIndexStream,
               builder: (context, snapshot) {
                 return MusicStateUtil.nextReturns<Widget>(
                   snapshot.data,
-                  audioPlayer,
+                  widget.audioPlayer,
                   noActive: _buttonBuilder(const Icon(Icons.skip_next_rounded), 60, (){}, locked: true),
-                  active: _buttonBuilder(const Icon(Icons.skip_next_rounded), 60, audioPlayer.seekToNext)
+                  active: _buttonBuilder(const Icon(Icons.skip_next_rounded), 60, widget.audioPlayer.seekToNext)
                 );
               })
             ),
@@ -100,5 +118,11 @@ class Controls extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _superToolTipController.dispose();
+    super.dispose();
   }
 }

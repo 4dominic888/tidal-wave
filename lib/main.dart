b6b29405ade557f9541773bbc43d1d0e3e3608f0
4dominic888 <4dominin888@gmail.com>
2024-05-21 10:20:37 -0500

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:tidal_wave/bloc/music_color_cubit.dart';
 import 'package:tidal_wave/bloc/music_cubit.dart';
 import 'package:tidal_wave/bloc/play_list_cubit.dart';
+import 'package:tidal_wave/firebase_options.dart';
 import 'package:tidal_wave/modules/lista_musica/screens/lista_musica_screen.dart';
 import 'package:tidal_wave/modules/reproductor_musica/classes/musica.dart';
 import 'package:tidal_wave/static_music.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   await JustAudioBackground.init(
     androidNotificationChannelId: 'com.ryanheise.bg_demon_channel.audio',
     androidNotificationChannelName: 'Audio playback',

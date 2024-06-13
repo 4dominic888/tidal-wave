e272d4ecadbcf8984f6fbf97255ed907b4f9068a
4dominic888 <4dominin888@gmail.com>
2024-06-12 21:25:16 -0500

    atributo de imagen

--- lib/modules/home_page/classes/music_list.dart
+++ lib/modules/home_page/classes/music_list.dart
@@ -1,12 +1,12 @@
 import 'package:tidal_wave/modules/reproductor_musica/classes/musica.dart';
 
-
 class MusicList {
   final String id;
   final String name;
   final String description;
+  final Uri? image;
   final String type;
   final List<Music> musics;
 
-  MusicList({required this.id, required this.name, required this.description, required this.type, required this.musics});
+  MusicList({required this.id, required this.name, required this.description, required this.type, required this.musics, this.image});
 }

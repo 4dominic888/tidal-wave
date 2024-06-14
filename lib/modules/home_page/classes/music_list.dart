09d3654d5dd8d8eec197ebf8bf768d8298d6a419
4dominic888 <4dominin888@gmail.com>
2024-06-14 08:23:07 -0500

    metodos auxiliares para leer y mandar jsons

--- lib/modules/home_page/classes/music_list.dart
+++ lib/modules/home_page/classes/music_list.dart
@@ -1,4 +1,4 @@
-import 'package:tidal_wave/modules/reproductor_musica/classes/musica.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MusicList {
   final String id;
@@ -6,7 +6,22 @@ class MusicList {
   final String description;
   final Uri? image;
   final String type;
-  final List<Music> musics;
+  final List<DocumentReference<Map<String, dynamic>>> musics;
 
   MusicList({required this.id, required this.name, required this.description, required this.type, required this.musics, this.image});
+
+  factory MusicList.fromJson(Map<String,dynamic> json, String uuid, String type) => MusicList(
+    id: uuid,
+    name: json['name'],
+    description: json['description'],
+    type: type,
+    //* TODA ESTA LINEA LARGOTA PARA RECUPERAR UNA SIMPLE LISTA DE REFERENCIAS, ESTUVE 4 DIAS SIN SABER COMO AAAAAAAAAAAAAA
+    musics: (json['musics'] as List).map<DocumentReference<Map<String, dynamic>>>((e) => FirebaseFirestore.instance.doc((e as DocumentReference).path)).toList()
+  );
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'description': description,
+    'musics': musics
+  };
 }

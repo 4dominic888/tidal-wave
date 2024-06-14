577c7573407b6668afa6995751ca2dab01c0938a
4dominic888 <4dominin888@gmail.com>
2024-06-14 08:24:15 -0500

    cambiando el origen de datos a la base de datos con firebase

--- lib/modules/home_page/screens/tw_user_list_nav.dart
+++ lib/modules/home_page/screens/tw_user_list_nav.dart
@@ -1,9 +1,11 @@
 import 'package:dotted_border/dotted_border.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:group_button/group_button.dart';
 import 'package:tidal_wave/modules/home_page/classes/music_list.dart';
 import 'package:tidal_wave/modules/home_page/screens/create_user_list_screen.dart';
 import 'package:tidal_wave/modules/home_page/widgets/tw_music_list_view_item.dart';
+import 'package:tidal_wave/services/repositories/tw_music_list_repository.dart';
 
 class TWUserListNav extends StatefulWidget {
   const TWUserListNav({super.key});
@@ -14,56 +16,73 @@ class TWUserListNav extends StatefulWidget {
 
 class _TWUserListNavState extends State<TWUserListNav> {
 
+  final _musicListRepo = TWMusicListRepository();
   static final _buttonsController = GroupButtonController(selectedIndex: 0);
   static final _buttonsOptions = ['Mis listas', 'Otras listas'];
-  final _finalListTest = <MusicList>[];
 
-  Widget _userList() => Expanded(
-    child: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CreateUserListScreen())),
-              splashColor: Colors.white,
-              child: Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                height: 60,
-                color: Colors.grey.shade900.withOpacity(0.9),
-                child: DottedBorder(
-                  color: Colors.grey.shade400,
-                  strokeCap: StrokeCap.butt,
-                  strokeWidth: 2,
-                  dashPattern: const [10,5],
-                  child: const ListTile(
-                    titleTextStyle: TextStyle(color: Colors.white),
-                    iconColor: Colors.white,
-                    leading: Icon(Icons.add),
-                    title: Text('Nueva Lista'),
-                  )
+  Future<List<MusicList>> _listOfMusic() async {
+    final result = await _musicListRepo.getAllListForUser(FirebaseAuth.instance.currentUser!.uid);
+    if(!result.onSuccess){
+      return [];
+    }
+    return result.data!;
+  }
+
+  Widget _userList() => 
+    Expanded(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CreateUserListScreen())),
+                splashColor: Colors.white,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  color: Colors.grey.shade900.withOpacity(0.9),
+                  child: DottedBorder(
+                    color: Colors.grey.shade400,
+                    strokeCap: StrokeCap.butt,
+                    strokeWidth: 2,
+                    dashPattern: const [10,5],
+                    child: const ListTile(
+                      titleTextStyle: TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      leading: Icon(Icons.add),
+                      title: Text('Nueva Lista'),
+                    )
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            shrinkWrap: true,
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            scrollDirection: Axis.vertical,
-            itemCount: _finalListTest.length,
-            itemBuilder: (context, index) {
-              final item = _finalListTest[index];
-              return TWMusicListViewItem(item: item);
-            },
+          Expanded(
+            child: FutureBuilder<List<MusicList>>(
+              future: _listOfMusic(),
+              builder: (context, snapshot) {
+                if(snapshot.connectionState == ConnectionState.waiting) return const Center(child: CircularProgressIndicator());
+                if(snapshot.hasError) return Center(child: Text(snapshot.error.toString(), style: const TextStyle(color: Colors.white)));
+                if(snapshot.data == null && snapshot.data!.isEmpty) return const Center(child: Text('No hay listas creadas', style: TextStyle(color: Colors.white)));
+                return ListView.builder(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  scrollDirection: Axis.vertical,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final item = snapshot.data![index];
+                    return TWMusicListViewItem(item: item);
+                  },
+                );
+              }
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
 
   final Widget _otherUserLists = const Center(
     child: Icon(Icons.list, color: Colors.white,),

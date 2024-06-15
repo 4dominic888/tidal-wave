5c62aecdcf2f713252b18256f145f410e942b606
4dominic888 <4dominin888@gmail.com>
2024-06-14 20:09:52 -0500

    parchando cosas

--- lib/modules/home_page/classes/music_list.dart
+++ lib/modules/home_page/classes/music_list.dart
@@ -10,18 +10,22 @@ class MusicList {
 
   MusicList({required this.id, required this.name, required this.description, required this.type, required this.musics, this.image});
 
+  MusicList.toSend({required this.name, required this.description, required this.type, this.image}): id = '', musics=[];
+
   factory MusicList.fromJson(Map<String,dynamic> json, String uuid, String type) => MusicList(
     id: uuid,
     name: json['name'],
     description: json['description'],
     type: type,
     //* TODA ESTA LINEA LARGOTA PARA RECUPERAR UNA SIMPLE LISTA DE REFERENCIAS, ESTUVE 4 DIAS SIN SABER COMO AAAAAAAAAAAAAA
-    musics: (json['musics'] as List).map<DocumentReference<Map<String, dynamic>>>((e) => FirebaseFirestore.instance.doc((e as DocumentReference).path)).toList()
+    musics: (json['musics'] as List).map<DocumentReference<Map<String, dynamic>>>((e) => FirebaseFirestore.instance.doc((e as DocumentReference).path)).toList(),
+    image: Uri.parse(json['image'])
   );
 
   Map<String, dynamic> toJson() => {
     'name': name,
     'description': description,
-    'musics': musics
+    'musics': musics,
+    'image': image.toString()
   };
 }

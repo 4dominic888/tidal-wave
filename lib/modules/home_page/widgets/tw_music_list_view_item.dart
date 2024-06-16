1a2ffbb31ce346f708d469a236e608f672deef77
4dominic888 <4dominin888@gmail.com>
2024-06-16 11:22:43 -0500

    cambiando el tap del iconbutton por todo el elemento

--- lib/modules/home_page/widgets/tw_music_list_view_item.dart
+++ lib/modules/home_page/widgets/tw_music_list_view_item.dart
@@ -23,68 +23,72 @@ class TWMusicListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey.withOpacity(0.4),
-      child: Container(
-        height: 110,
-        padding: const EdgeInsets.all(0),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 6,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: item.image != null ? Image.network(item.image.toString()).image : Image.asset('assets/placeholder/music-placeholder.png').image,
-                    fit: BoxFit.cover
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () async {
+          final result = await TWMusicRepository().getAllByReferences(item.musics);
+          if(!context.mounted) return;
+          if(!context.mounted) return;
+          if(!result.onSuccess){
+            showDialog(context: context, builder: (context) => PopupMessage(
+              title: 'Error',
+              description: result.errorMessage!
+            ));
+            return;
+          }
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ListaMusicaScreen(listado: result.data!)));
+        },
+        splashColor: Colors.white,
+        child: Card(
+          color: Colors.grey.withOpacity(0.4),
+          child: Container(
+            height: 110,
+            padding: const EdgeInsets.all(0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: item.image != null ? Image.network(item.image.toString()).image : Image.asset('assets/placeholder/music-placeholder.png').image,
+                        fit: BoxFit.cover
+                      )
+                    ),
                   )
                 ),
-              )
-            ),
-            const Spacer(flex: 1),
-            Expanded(
-              flex: 10,
-              child: Container(
-                padding: const EdgeInsets.only(top: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Text(item.name, style: const TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
-                    ),
-                    Icon(typeIcon(item.type), color: Colors.white, size: 15),
-                    Text('${item.musics.length} canciones', style: TextStyle(color: Colors.grey.shade200)),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(bottom: 10.0, top: 4.0),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Text(item.description, style: TextStyle(color: Colors.grey.shade400, fontSize: 13), textAlign: TextAlign.justify,)
+                const Spacer(flex: 1),
+                Expanded(
+                  flex: 10,
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Text(item.name, style: const TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold))
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              )
+                        Icon(typeIcon(item.type), color: Colors.white, size: 15),
+                        Text('${item.musics.length} canciones', style: TextStyle(color: Colors.grey.shade200)),
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.only(bottom: 10.0, top: 4.0),
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: Text(item.description, style: TextStyle(color: Colors.grey.shade400, fontSize: 13), textAlign: TextAlign.justify,)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                )
+              ],
             ),
-            Expanded(
-              flex: 4,
-              child: IconButton(icon: const Icon(Icons.play_arrow, color: Colors.white), onPressed: () async {
-                final result = await TWMusicRepository().getAllByReferences(item.musics);
-                if(!context.mounted) return;
-                if(!result.onSuccess){
-                  showDialog(context: context, builder: (context) => PopupMessage(
-                    title: 'Error',
-                    description: result.errorMessage!
-                  ));
-                  return;
-                }
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => ListaMusicaScreen(listado: result.data!)));
-              })
-            )
-          ],
+          ),
         ),
       ),
     );

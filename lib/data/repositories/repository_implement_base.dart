49c1c229c60b5ad23e55d43955362ccdbd41c2a3
4dominic888 <4dominin888@gmail.com>
2024-06-17 21:19:55 -0500

    cambiando estructura de base de datos, me di cuenta que firestore puede usar persistencia offline.

--- lib/data/repositories/repository_implement_base.dart
+++ lib/data/repositories/repository_implement_base.dart
@@ -1,60 +1,19 @@
 import 'package:tidal_wave/data/dataSources/firebase/firestore_database_service.dart';
 import 'package:tidal_wave/data/repositories/database_service.dart';
 
-enum TypeDataBase{
-  /// Base de datos online en la nube, backend de google
-  firestore,
-  /// Base de datos local del dispositivo
-  hive
-}
+abstract class RepositoryImplementBase {
+  late final DatabaseService<Map<String,dynamic>> _context;
 
-abstract class RepositoryImplementBase<T> {
-  late DatabaseService<Map<String,dynamic>> _context;
-
-  /// Tipo de base de datos seleccionado
-  TypeDataBase type;
+  /// Obtiene la instancia general de la base de datos
+  DatabaseService get context => _context;
 
   /// Nombre de la coleccion o tabla a hacer referencia
   String get dataset;
 
-  /// Obtiene la instancia general de la base de datos
-  DatabaseService get generalContext => _context;
-
-  /// Accion a realizar dependiendo de la base de datos
-  void actionDependingToDB({
-    required void Function(FirestoreDatabaseService firestoreContext) isFireStore,
-    required void Function() isHive
-  }
-  ){
-    switch (type) {
-      case TypeDataBase.firestore: {
-        final context = _context as FirestoreDatabaseService;
-        isFireStore.call(context);
-        break;
-      }
-
-      case TypeDataBase.hive: isHive.call(); break;
-      default: throw Exception('Ninguna base de datos seleccionada');
-    }
-  }
-
-  RepositoryImplementBase(this.type){
-    switch (type) {
-      case TypeDataBase.firestore: _context = FirestoreDatabaseService(); break;
-      default: throw Exception('Tipo de base de datos no valido');
-    }
-  }
+  RepositoryImplementBase(DatabaseService<Map<String,dynamic>> context) :  _context = context;
+  RepositoryImplementBase.firestore() : _context = FirestoreDatabaseService();
 }
 
-mixin OnlyFirestoreAction<T> on RepositoryImplementBase<T> {
-  void actionOnlyFirestore(void Function(FirestoreDatabaseService firestoreContext) action){
-    switch (type) {
-      case TypeDataBase.firestore: {
-        final context = _context as FirestoreDatabaseService;
-        action.call(context);
-        break;
-      }
-      default: throw Exception('Esta funcion solo esta disponible para la base de datos de firebase');
-    }    
-  }
+mixin UseFirestore on RepositoryImplementBase{
+  FirestoreDatabaseService get firestoreContext => context as FirestoreDatabaseService;
 }
--- lib/data/repositories/user_repository_implement.dart
+++ lib/data/repositories/user_repository_implement.dart
@@ -5,8 +5,8 @@ import 'package:tidal_wave/domain/repositories/user_repository.dart';
 
 typedef T = TWUser;
 
-class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirestoreAction<T> implements UserRepository{
-  UserRepositoryImplement(super.type);
+class UserRepositoryImplement extends RepositoryImplementBase with UseFirestore implements UserRepository{
+  UserRepositoryImplement() : super.firestore();
 
   @override
   String get dataset => 'Users';
@@ -14,8 +14,7 @@ class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirest
   @override
   Future<Result<List<T>>> getAll({List<String> queryArray = const [], bool Function(Map<String, dynamic>)? where, int limit = 10}) async{
     try {
-      late final List<Map<String, dynamic>> data;
-      actionOnlyFirestore((firestoreContext) async => data = await firestoreContext.getAll(dataset, queryArray, where, limit));
+      final data = await firestoreContext.getAll(dataset, queryArray, where, limit);
       return Result.sucess(data.map((e) => T.fromJson(e)).toList());
     } on Exception catch (e) {
       return Result.error('Ha ocurrido un error: $e');
@@ -25,13 +24,11 @@ class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirest
   @override
   Future<Result<T>> getOne(String id, [List<String> queryArray = const []]) async {
     try {
-      late final Map<String, dynamic>? data;
-      actionOnlyFirestore((firestoreContext) async => data = await firestoreContext.getOne(dataset, id, queryArray));
-
+      final data = await firestoreContext.getOne(dataset, id, queryArray);
       if (data == null) {
         return Result.error('No se ha encontrado el elemento');
       }
-      return Result.sucess(T.fromJson(data!));
+      return Result.sucess(T.fromJson(data));
     } on Exception catch (e) {
       return Result.error('Ha ocurrido un error: $e');
     }
@@ -40,13 +37,11 @@ class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirest
   @override
   Future<Result<T>> addOne(T data, String? id, [List<String> queryArray = const []]) async {
     try {
-      actionOnlyFirestore((firestoreContext) async {
-        if(id == null){
-          await firestoreContext.addOne(dataset, data.toJson(), queryArray);
-        } else{
-          await firestoreContext.setOne(dataset, data.toJson(), id, queryArray);
-        }
-      });
+      if(id == null){
+        await firestoreContext.addOne(dataset, data.toJson(), queryArray);
+      } else{
+        await firestoreContext.setOne(dataset, data.toJson(), id, queryArray);
+      }
       return Result.sucess(data);
     } on Exception catch (e) {
       return Result.error('Ha ocurrido un error: $e');
@@ -56,8 +51,7 @@ class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirest
   @override
   Future<Result<T>> updateOne(T data, String id, [List<String> queryArray = const []]) async {
     try {
-      late final Map<String, dynamic> retorno;
-      actionOnlyFirestore((firestoreContext) async => retorno = await firestoreContext.updateOne(dataset, data.toJson(), id, queryArray));
+      final retorno = await firestoreContext.updateOne(dataset, data.toJson(), id, queryArray);
       return Result.sucess(T.fromJson(retorno));
     } on Exception catch (e) {
       return Result.error('Ha ocurrido un error: $e');
@@ -67,7 +61,7 @@ class UserRepositoryImplement extends RepositoryImplementBase<T> with OnlyFirest
   @override
   Future<Result<String>> deleteOne(String id, [List<String> queryArray = const []]) async {
     try {
-      actionOnlyFirestore((firestoreContext) async => await firestoreContext.deleteOne(dataset, id, queryArray));
+      firestoreContext.deleteOne(dataset, id, queryArray);
       return Result.sucess(id);
     } on Exception catch (e) {
       return Result.error('Ha ocurrido un error: $e');

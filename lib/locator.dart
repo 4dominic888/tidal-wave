db96b4e2aa3c88f63e0c1c6f2f9cef0686178c5f
4dominic888 <4dominin888@gmail.com>
2024-06-26 22:30:37 -0500

    registro de bases de datos de firebase y sqflite, ademas de un cambio de nombre

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,16 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:tidal_wave/data/abstractions/connectivity_service_base.dart';
+import 'package:tidal_wave/data/abstractions/database_service.dart';
 import 'package:tidal_wave/data/dataSources/firebase/firestore_database_service.dart';
 import 'package:tidal_wave/data/dataSources/firebase/firestore_actions_connectivty_service_implement.dart';
+import 'package:tidal_wave/data/dataSources/sqflite/sqflite_database_service.dart';
 import 'package:tidal_wave/data/repositories/music_list_repository_implement.dart';
 import 'package:tidal_wave/data/repositories/music_repository_implement.dart';
 import 'package:tidal_wave/data/repositories/user_repository_implement.dart';
 import 'package:tidal_wave/domain/use_case/implementations/authentication_manager_use_case_implement.dart';
 import 'package:tidal_wave/domain/use_case/implementations/music_manager_use_case_implement.dart';
-import 'package:tidal_wave/domain/use_case/implementations/play_list_manager_use_case_implement.dart';
+import 'package:tidal_wave/domain/use_case/implementations/music_list_manager_use_case_implement.dart';
 import 'package:tidal_wave/domain/use_case/interfaces/authentication_manager_use_case.dart';
 import 'package:tidal_wave/domain/use_case/interfaces/music_manager_use_case.dart';
-import 'package:tidal_wave/domain/use_case/interfaces/play_list_manager_use_case.dart';
+import 'package:tidal_wave/domain/use_case/interfaces/music_list_manager_use_case.dart';
 
 final GetIt locator = GetIt.instance;
 
@@ -18,6 +20,14 @@ void setupLocator() {
 
   locator.registerLazySingleton<ConnectivityServiceBase>(() =>
     FirestoreActionsConnectityServiceImplement()  
+  );
+
+  locator.registerLazySingleton<DatabaseService<Map<String,dynamic>>>(
+    () => FirestoreDatabaseService(), instanceName: 'Firestore'
+  );
+
+  locator.registerLazySingleton<DatabaseService<Map<String,dynamic>>>(
+    () => SqfliteDatabaseService(), instanceName: 'Sqflite'
   );
 
   locator.registerLazySingleton<AuthenticationManagerUseCase>(() => 
@@ -32,8 +42,8 @@ void setupLocator() {
     ))
   );
   
-  locator.registerLazySingleton<PlayListManagerUseCase>(() => 
-    PlayListManagerUseCaseImplement(MusicListRepositoryImplement(
+  locator.registerLazySingleton<MusicListManagerUseCase>(() => 
+    MusicListManagerUseCaseImplement(MusicListRepositoryImplement(
       databaseService: FirestoreDatabaseService()
     ))
   );

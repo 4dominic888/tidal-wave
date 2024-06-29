0543f35eb7508e7ee7aab544a81e0743d3f4e3aa
4dominic888 <4dominin888@gmail.com>
2024-06-28 19:51:49 -0500

    cambiando buttonController a final, quitando static por un bug

--- lib/presentation/pages/home_page/find_music_nav/screens/tw_find_nav.dart
+++ lib/presentation/pages/home_page/find_music_nav/screens/tw_find_nav.dart
@@ -26,7 +26,7 @@ class _TWFindNavState extends State<TWFindNav> {
   String? selectUUID;
   DataSourceType _selectedType = DataSourceType.online;
 
-  static final _buttonsController = GroupButtonController(selectedIndex: 0);
+  final _buttonsController = GroupButtonController(selectedIndex: 0);
   static final _buttonsOptions = ['Musicas publicas', 'Mis musicas descargadas'];
 
 
@@ -44,7 +44,10 @@ class _TWFindNavState extends State<TWFindNav> {
   }
 
   void _scrollListener(){
-    if(!_loading && _hasMore && _scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+  final double viewportExtent = _scrollController.position.viewportDimension;
+  final double offsetActivation = 0.8 * viewportExtent; //* 80% de viewport
+
+    if(!_loading && _hasMore && _scrollController.position.pixels >= _scrollController.position.maxScrollExtent - offsetActivation){
       _fetchData(_selectedType);
     }
   }
@@ -56,10 +59,10 @@ class _TWFindNavState extends State<TWFindNav> {
     _lastItem = _allData.isNotEmpty ? _allData.last : null;
 
     final newData = await listOfMusic(dataSourceType, lastItem: _lastItem);
+    _allData.addAll(newData);
+    if(newData.length < _limit){_hasMore = false;}
+    _loading = false;
     setState(() {
-      _allData.addAll(newData);
-      if(newData.length < _limit){_hasMore = false;}
-      _loading = false;
     });
   }
 
@@ -90,120 +93,103 @@ class _TWFindNavState extends State<TWFindNav> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      controller: _scrollController,
-      slivers: [
-
-        //* Barra de busqueda
-        SliverAppBar(
-          automaticallyImplyLeading: false,
-          toolbarHeight: 80,
-          backgroundColor: Colors.transparent,
-          actions: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: TextFieldFind(
-                  hintText:'Buscar cancion...',
-                  suffixIcon: IconButtonUIMusic(
-                    borderColor: Colors.blue.shade400.withAlpha(50),
-                    borderSize: 2.0,
-                    fillColor: Colors.transparent,
-                    icon: const Icon(Icons.search, size: 25),
-                    onTap: () {},
+    return Scrollbar(
+      child: CustomScrollView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        controller: _scrollController,
+        slivers: [
+      
+          //* Barra de busqueda
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            toolbarHeight: 80,
+            backgroundColor: Colors.transparent,
+            actions: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFieldFind(
+                    hintText:'Buscar cancion...',
+                    suffixIcon: IconButtonUIMusic(
+                      borderColor: Colors.blue.shade400.withAlpha(50),
+                      borderSize: 2.0,
+                      fillColor: Colors.transparent,
+                      icon: const Icon(Icons.search, size: 25),
+                      onTap: () {},
+                    ),
+                    onChanged: _findMusic
                   ),
-                  onChanged: _findMusic
-                ),
-              )
-            ),
-          ],
-        ),
-    
-        SliverToBoxAdapter(child: 
-        
-        //* Seleccionar entre 2 opciones
-        GroupButton(
-          controller: _buttonsController,
-          buttonIndexedBuilder: (selected, index, context) => ElevatedButton(
-            onPressed: (){
-              setState(() {
-                _buttonsController.selectIndex(index);
-                _allData.clear();
-                _lastItem = null;
-                _hasMore = true;
-                _loading = false;
-                _selectedType = index == 0 ? DataSourceType.online : DataSourceType.local;
-                _fetchData(_selectedType);
-              });
-            },
-            style: ButtonStyle(
-              backgroundColor: WidgetStateColor.resolveWith((states) => selected ? const Color.fromARGB(255, 36, 161, 196) : const Color.fromARGB(255, 20, 84, 101))
-            ),
-            child: Text(_buttonsOptions[index], style: TextStyle(color: selected ? Colors.white : Colors.grey.shade300))
+                )
+              ),
+            ],
           ),
-          isRadio: true,
-          buttons: _buttonsOptions
-        )
-        ),
-
-        const SliverToBoxAdapter(child: SizedBox(height: 10)),
-    
-        FutureBuilder(
-          future: _fetchData(_selectedType),
-          builder: (context, snapshot) {
-            if(snapshot.connectionState == ConnectionState.waiting) {return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));}
-            if(snapshot.hasError) {return SliverToBoxAdapter(child: Center(child: Text('Ha ocurrido un error ${snapshot.error.toString()}')));}
-            if(_allData.isEmpty) {return const SliverToBoxAdapter(child: Center(child: Text('No hay canciones por el momento')));}
-            return StatefulBuilder(
-              builder: (context, setState) {
-                return SliverGrid(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    childAspectRatio: 1
-                  ),
-                  delegate: SliverChildBuilderDelegate(childCount: _allData.length + (_hasMore ? 1 : 0), (context, index) {
-                    final item = _allData[index];
-                    if(index < _allData.length){
-                      return Builder(
-                        builder: (context) {
-                          return MusicElementView(
-                            item: item,
-                            selected: [item.uuid == selectUUID],
-                            onPlay: () {
-                              selectUUID = item.uuid;
-                              setState(() {});
-                            },
-                            isOnline: _selectedType == DataSourceType.online,
-                          );
-                        }
-                      );
-                    }
-                    else{
-                      if(_hasMore){
-                        return const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: CircularProgressIndicator(),
+      
+          //* Seleccionar entre 2 opciones
+          SliverToBoxAdapter(child: 
+            GroupButton(
+              controller: _buttonsController,
+              buttonIndexedBuilder: (selected, index, context) => ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    _buttonsController.selectIndex(index);
+                    _allData.clear();
+                    _lastItem = null;
+                    _hasMore = true;
+                    _loading = false;
+                    _selectedType = index == 0 ? DataSourceType.online : DataSourceType.local;
+                    _fetchData(_selectedType);
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateColor.resolveWith((states) => selected ? const Color.fromARGB(255, 36, 161, 196) : const Color.fromARGB(255, 20, 84, 101))
+                ),
+                child: Text(_buttonsOptions[index], style: TextStyle(color: selected ? Colors.white : Colors.grey.shade300))
+              ),
+              isRadio: true,
+              buttons: _buttonsOptions
+            )
+          ),
+      
+          const SliverToBoxAdapter(child: SizedBox(height: 10)),
+          Builder(
+            builder: (context) {
+              if(_allData.isEmpty) {return const SliverToBoxAdapter(child: Center(child: Text('No hay canciones por el momento')));}
+              return StatefulBuilder(
+                builder: (context, setState) {
+                  return SliverGrid(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 1
+                    ),
+                    delegate: SliverChildListDelegate(
+                      _allData.map((e) => Builder(builder: (context) {
+                        return MusicElementView(
+                          item: e,
+                          selected: [e.uuid == selectUUID],
+                          onPlay: () {
+                            selectUUID = e.uuid;
+                            setState(() {});
+                          },
+                          isOnline: _selectedType == DataSourceType.online
                         );
-                      }
-                      else {return const SizedBox.shrink();}
-                    }
-                  }),
-                );
-              }
-            );
-          }
-        )
-      ],
+                      })).toList()
+                  ),
+                  );
+                }
+              );
+            }
+          )
+        ],
+      ),
     );
   }
 
   @override
   void dispose() {
-    super.dispose();
     _scrollController.removeListener(_scrollListener);
     _scrollController.dispose();
+    super.dispose();
   }
 }

7d407af76cbe002bf77b692735d1db1f060a759f
4dominic888 <4dominin888@gmail.com>
2024-07-09 21:31:05 -0500

    import redundante

--- lib/presentation/utils/function_utils.dart
+++ lib/presentation/utils/function_utils.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tidal_wave/data/abstractions/tw_enums.dart';
 
 Duration parseDuration(String s) {

f588f52ba45de3852529936bae3ee5c0bdf9177d
4dominic888 <4dominin888@gmail.com>
2024-07-12 17:55:29 -0500

    AJustando el home

--- lib/presentation/pages/home_page/home_nav/screens/tw_home_nav.dart
+++ lib/presentation/pages/home_page/home_nav/screens/tw_home_nav.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tidal_wave/presentation/pages/home_page/home_nav/widgets/ad_container.dart';
-import 'package:tidal_wave/presentation/pages/reproductor_musica/widgets/media_meta_data.dart';
 
 class TWHomeNav extends StatelessWidget {
   const TWHomeNav({super.key});
@@ -8,46 +7,18 @@ class TWHomeNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
       child: Column(
         children: [
-          const AdContainer(
+          AdContainer(
             text: 'Algun anuncio',
             description: 'Alguna descripcion',
             gradient: LinearGradient(colors: [Colors.blueAccent, Colors.pink]),
           ),
 
-          const SizedBox(height: 30),
+          SizedBox(height: 30),
 
-          const Text('Musicas destacadas', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-          const SizedBox(height: 10),
-
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Wrap(
-              children: List.generate(8, (_) => 
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: (){},
-                      highlightColor: Colors.white,
-                      child: const MediaMetaData(
-                        sizePercent: 0.5,
-                          title: 'title',
-                          artist: 'artist',
-                          color: Colors.white
-                        ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 10),
-          const Text('Under in construction...', style: TextStyle(fontSize: 20)),
+          Text('Bienvenido a Tidal Wave', style: TextStyle(fontSize: 20)),
         ],
       ),
     );
